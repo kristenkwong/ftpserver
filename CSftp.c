906ba26f07013e001cacef58ae1d7d4b0af9dd0c
alexanderluong <alexander.luong@hotmail.com>
2019-03-28 14:10:05 -0700

    Started to create socket

--- CSftp.c
+++ CSftp.c
@@ -1,6 +1,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include "dir.h"
 #include "usage.h"
 
@@ -8,23 +11,37 @@
 // one how to get the arguments passed on the command line.
 
 int main(int argc, char *argv[]) {
-
-    // This is some sample code feel free to delete it
-    // This is the main program for the thread version of nc
-
-    int i;
-    
     // Check the command line arguments
     if (argc != 2) {
       usage(argv[0]);
       return -1;
     }
 
+    // get port number from argument passed in 
+    int port_num = atoi(argv[1]);
+    // check if port number is valid
+    if (port_num == 0) {
+      printf("Invalid port number.\n");
+      return -1;
+    }
+    printf("Port number: %d\n", port_num);
+
+    struct addrinfo;
+    // create a socket
+    int socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_descriptor == -1) {
+      printf("Socket failed to be created.\n");
+      return -1;
+    }
+    printf("Socket fd: %d\n", socket_descriptor);
+
+
+
     // This is how to call the function in dir.c to get a listing of a directory.
     // It requires a file descriptor, so in your code you would pass in the file descriptor 
     // returned for the ftp server's data connection
     
-    printf("Printed %d directory entries\n", listFiles(1, "."));
+    // printf("Printed %d directory entries\n", listFiles(1, "."));
     return 0;
 
 }
