327be11b039fa06b3cf3563e50b3236cab09abb3
alexanderluong <alexander.luong@hotmail.com>
2019-03-30 15:35:40 -0700

    Implement quit command. Add more comments

--- CSftp.c
+++ CSftp.c
@@ -12,16 +12,19 @@
 // Here is an example of how to use the above function. It also shows
 // one how to get the arguments passed on the command line.
 
-void not_logged_in_response(int fd) {
+void not_logged_in_response(int fd)
+{
   send(fd, "530 Not logged in.\r\n", strlen("530 Not logged in.\r\n"), 0);
 }
 
-void syntax_error_response(int fd) {
+void syntax_error_response(int fd)
+{
   send(fd, "500 Syntax error, command unrecognized.\r\n",
-           strlen("500 Syntax error, command unrecognized.\r\n"), 0);
+       strlen("500 Syntax error, command unrecognized.\r\n"), 0);
 }
 
-void syntax_error_args_response(int fd) {
+void syntax_error_args_response(int fd)
+{
   send(fd, "501 Syntax error in parameters or arguments.\r\n", strlen("501 Syntax error in parameters or arguments.\r\n"), 0);
 }
 
@@ -85,15 +88,11 @@ int main(int argc, char *argv[])
   send(new_socket_fd, "220 Service ready for new user.\r\n", strlen("220 Service ready for new user.\r\n"), 0);
   while (1)
   {
-    // TODO: finish the user sequence.
-    // plan: make an int to determine if user is already logged in or not
-    // check for USER command (need to split strings)
-    // check for argument counts and that it's cs317
-    // if not logged in, reject other commands
     int data_len = recv(new_socket_fd, buffer, 1024, 0);
+    printf("Data length: %d\n", data_len);
     if (data_len <= 0)
     {
-      break;
+      continue;
     }
     else if (data_len > 1024)
     // overflow detection
@@ -105,6 +104,7 @@ int main(int argc, char *argv[])
     char delimit[] = " \t\r\n\v\f";
 
     // first string
+    // TODO: null check if there's an empty line recevied from the client
     command = strtok(buffer, delimit);
 
     // loop through all the tokens
@@ -114,8 +114,8 @@ int main(int argc, char *argv[])
     //   token = strtok(NULL, s);
     // }
 
-    printf("%s\n", command);
-
+    printf("The first command: %s\n", command);
+    // TODO: should probably implement a more robust way of checking for arg count
     // START PROCESSING COMMANDS FROM THE USER
     if (logged_in == 0 && strcasecmp(command, "user") != 0)
     {
@@ -127,42 +127,66 @@ int main(int argc, char *argv[])
     {
       if (strcasecmp(command, "user") == 0)
       {
+        char *second_arg;
         if (command != NULL)
         {
           // get the second argument
-          command = strtok(NULL, delimit);
-          printf("the second arg: %s\n", command);
-          if (command == NULL) {
-            // if you enter user and there's no other arguments (NULL)
+          second_arg = strtok(NULL, delimit);
+          printf("The second arg: %s\n", second_arg);
+          if (second_arg == NULL)
+          {
+            // if you enter user and there's no other arguments (NULL) 
             syntax_error_args_response(new_socket_fd);
             continue;
           }
         }
-        if (strcasecmp(command, "cs317") == 0)
+        if (strcasecmp(second_arg, "cs317") == 0)
         {
+          printf("%s\n", "Login was successful. Setting logged_in = 1");
           logged_in = 1;
           send(new_socket_fd, "230 User logged in, proceed.\r\n", strlen("230 User logged in, proceed.\r\n"), 0);
         }
+        else
+        {
+          not_logged_in_response(new_socket_fd);
+          continue;
+        }
       }
-      else if (strcasecmp(command, "quit") == 0) {
-
-      } else if (strcasecmp(command, "cwd") == 0) {
-        
-      } else if (strcasecmp(command, "cdup") == 0) {
-
-      } else if (strcasecmp(command, "type") == 0) {
-
-      } else if (strcasecmp(command, "mode") == 0) {
-
-      } else if (strcasecmp(command, "stru") == 0) {
-
-      } else if (strcasecmp(command, "retr") == 0) {
-
-      } else if (strcasecmp(command, "pasv") == 0) {
-
-      } else if (strcasecmp(command, "nlst") == 0) {
-
-      } else {
+      else if (strcasecmp(command, "quit") == 0)
+      {
+        logged_in = 0;
+        send(new_socket_fd, "221 Service closing control connection.\r\n", 
+                strlen("221 Service closing control connection.\r\n"), 0);
+        close(new_socket_fd);
+        // TODO: NOT SURE IF I NEED TO RETURN A 0 HERE TO END THE PROGRAM
+        return 0;
+      }
+      else if (strcasecmp(command, "cwd") == 0)
+      {
+      }
+      else if (strcasecmp(command, "cdup") == 0)
+      {
+      }
+      else if (strcasecmp(command, "type") == 0)
+      {
+      }
+      else if (strcasecmp(command, "mode") == 0)
+      {
+      }
+      else if (strcasecmp(command, "stru") == 0)
+      {
+      }
+      else if (strcasecmp(command, "retr") == 0)
+      {
+      }
+      else if (strcasecmp(command, "pasv") == 0)
+      {
+      }
+      else if (strcasecmp(command, "nlst") == 0)
+      {
+      }
+      else
+      {
         // command isn't one of the ones supported
         syntax_error_response(new_socket_fd);
       }
